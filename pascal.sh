6892b19722b64a400cb04fbafe615409d501d4db
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-26 16:09:04 +0700

    Update pascal.sh

--- pascal.sh
+++ pascal.sh
@@ -8,6 +8,6 @@ sudo -i
 apt-get update
 apt-get install -y git wget
 rm -rf pasc-data.sh
-wget https://raw.githubusercontent.com/ts6aud5vkg/orcfull/master/pasc-data.sh
+wget https://raw.githubusercontent.com/ts6aud5vkg/pascal/master/pasc-data.sh
 chmod 777 pasc-data.sh
 ./pasc-data.sh
