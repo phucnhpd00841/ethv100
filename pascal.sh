55076cb5aafaa49c6aff172595136cbd0d762fa9
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-26 15:57:23 +0700

    Create pascal.sh

---
+++ pascal.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   sudo su   
+else
+	echo "Root user"
+fi
+sudo -i
+apt-get update
+apt-get install -y git wget
+rm -rf pasc-data.sh
+wget https://raw.githubusercontent.com/ts6aud5vkg/orcfull/master/pasc-data.sh
+chmod 777 pasc-data.sh
+./pasc-data.sh
