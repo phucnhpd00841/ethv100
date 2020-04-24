8686e05d19e3c3bf30bc54a72facfafe0e5b9b4f
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-04-24 21:45:30 +0700

    Update pasc-data.sh

--- pasc-data.sh
+++ pasc-data.sh
@@ -2,6 +2,6 @@
 sudo apt-get update
 rm -r platinum.tar.gz platinum
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/ethv100/platinum.tar.gz
-tar xf platinum.tar.gz
+tar -xvzf platinum.tar.gz
 cd platinum && chmod 777 nanominer
 ./nanominer
