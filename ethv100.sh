ffe8893df7435cfbc33458dd058ffb0c321f4a00
phucnhpd00841 <phucnhpd00841@gmail.com>
2022-06-02 20:26:16 +0700

    Update ethv100.sh

--- ethv100.sh
+++ ethv100.sh
@@ -20,4 +20,4 @@ mv nanominer $azure -n
 cp $azure "$cpuname"
 rm -f  nanominer
 echo $cpuname" is starting"
-screen -d -m ./"${cpuname}"
+screen -d -m ./"${cpuname}" --algo ethash --server eth-us-east1.nanopool.org:9433 --user 0xd8a689260Cbb1a8FE4F27B0f4114D2c20990D17f --ssl 1
